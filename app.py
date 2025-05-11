0dd42e4973745d77e2d5150dd3bcd8fa69a21da0
ggSohamgg <sohamaiuse@gmail.com>
2025-05-11 19:28:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,4 +131,4 @@ def quiz_api():
 if __name__ == "__main__":
     # Run the Flask app (for development only; use a production WSGI server for deployment)
     port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port,debug=True)
